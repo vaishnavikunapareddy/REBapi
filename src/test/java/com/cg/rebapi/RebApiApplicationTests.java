61a0a00d2a6fca0e9c85096869b995557f757312
HP <vaishu.kunapareddy@gmail.com>
2021-09-17 17:53:21 +0530

    initial changes

---
+++ src/test/java/com/cg/rebapi/RebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.rebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
