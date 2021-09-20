7f73191bc1fda9bb173725b58a8bcddbf8d86c79
HP <vaishu.kunapareddy@gmail.com>
2021-09-20 22:19:02 +0530

    vaishnavi third commit

--- src/main/java/com/cg/rebapi/repository/ShopRepository.java
+++ src/main/java/com/cg/rebapi/repository/ShopRepository.java
@@ -1,9 +1,10 @@
 package com.cg.rebapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.rebapi.model.Shop;
-
-public interface ShopRepository extends JpaRepository<Shop, Integer>{
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long>{
 
 }
